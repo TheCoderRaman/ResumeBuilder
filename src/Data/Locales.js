cdfc1aa7a412dcca9be2be1d2d2edaa16099db10
Raman Verma <devramanverma@gmail.com>
2023-05-31 23:31:22 +0530

    Added translator translations

---
+++ src/Data/Locales.js
@@ -0,0 +1,19 @@
+/**
+ * This module contain all translations
+ * used in the entire application 
+ * in the various places.
+ * 
+ * @var const Object Locales
+ */
+export const Locales = {
+    config: {
+        lng: 'en',fallbackLng: 'en'
+    },
+    resources: {
+        en: {
+            translation: {
+                //
+            }
+        }
+    }
+};
