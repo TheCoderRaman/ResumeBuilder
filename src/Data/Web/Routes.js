9183f9cd8fa01d4f6d1e23db2014464da88698ae
Raman Verma <devramanverma@gmail.com>
2023-05-31 23:29:36 +0530

    Added application routes collection

---
+++ src/Data/Web/Routes.js
@@ -0,0 +1,24 @@
+/**
+ * This module give us a single location for altering any
+ * route in the entire application at any point of 
+ * time in the future. if we wanted too! ^_*.
+ * 
+ * @var const Object Web
+ */
+export const Web = {
+    Home: {
+        path: '/'
+    },
+    AboutPage: {
+        path: 'about-us'
+    },
+    Templates: {
+        path: 'templates'
+    },
+    GeneratedResume: {
+        path: 'template/resume'
+    },
+    FillDetails: {
+        path: 'template/fill-details'
+    }
+};
