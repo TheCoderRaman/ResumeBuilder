897706ca7ece0239e125c83dff68c0a32896fd0f
Raman Verma <devramanverma@gmail.com>
2023-05-31 23:28:17 +0530

    Added routing support

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import Page from './Layouts/Page';
+import { Web } from './Data/Web/Routes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
     return (
-        <>Hello React</>
+        <Router>
+            <Routes>
+                {/* Here we register all of our web defined in the routes module. */}
+                {Object.entries(Web).map(([name,props],index) => {
+                    // Here: we will create new route with the stored 
+                    // information in the web routes module.
+                    return <Route 
+                        extact
+                        // Assign route
+                        path={props.path ?? null} 
+                        // Assign element 
+                        element={<Page>
+                            {props.element ?? (
+                                // Fallback element incase route does not 
+                                // have any valid element to show or
+                                // specified in the web routes.
+                                <>{name}</>
+                            )}
+                        </Page>} 
+                    />;
+                })}
+            </Routes>
+        </Router>
     )
 };
 
