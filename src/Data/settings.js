3711d20b14fecd14d8ece704d78b72fdc77fdbda
Raman Verma <devramanverma@gmail.com>
2023-07-17 18:51:50 +0530

    Added setting file

---
+++ src/Data/settings.js
@@ -0,0 +1,12 @@
+/**
+ * All application settings will be defined here.
+ * 
+ * @var const settings
+ */
+export const settings = {
+    app: {
+        name: 'Resume Builder',
+        shorName: 'ResumeBuilder',
+        summary: 'React app for creating resume'
+    }
+};
