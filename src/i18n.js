54148ff57159773c4f7042f9c4fe1c6ddca2b5b9
Raman Verma <devramanverma@gmail.com>
2023-07-17 19:00:57 +0530

    Added caching feature for current locale in client browser

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,33 @@
 import i18n from 'i18next';
-import { Locales } from './Data/Locales';
-import { initReactI18next } from 'react-i18next';
+import { initReactI18next } from "react-i18next";
+import { config, resources } from "./Data/translation";
+import { localStorage as ls} from "./Modules/LocalStorageSlim";
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export default function setUp() {
-    const resources = Locales.resources;
+config.selected = ls.get('locale',{ 
+    decrypt: true 
+});
 
-    i18n.use(LanguageDetector).use(initReactI18next).init({
-        resources,
-        lng: Locales.config.lng,
-        interpolation: {
-            escapeValue: false
-        },
-        fallbackLng: Locales.config.fallbackLng,
-    });
+config.selected = (
+    (config.selected === null) ? config.lng : config.selected
+);
 
-    document.documentElement.setAttribute('lang',
-        Locales.config.lng
-    );
-    i18n.on('languageChanged', (lng) => {
-        document.documentElement.setAttribute('lang', lng);
-    });
-}
+i18n.use(LanguageDetector).use(initReactI18next).init({
+    resources,
+    lng: config.selected,
+    interpolation: {
+        escapeValue: false
+    },
+    fallbackLng: config.fallbackLng,
+});
+
+document.documentElement.setAttribute(
+    'lang',config.selected
+);
+
+i18n.on('languageChanged', (lng) => {
+    ls.set('locale',lng,{ encrypt: false });
+    document.documentElement.setAttribute('lang', lng);
+});
+
+export default i18n;
