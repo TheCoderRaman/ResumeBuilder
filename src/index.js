9dda1306dcd31062111505151ebab38fd4b73462
Raman Verma <devramanverma@gmail.com>
2023-07-18 13:19:45 +0530

    Updated redux store path

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import App from "./App";
 import React from "react";
-import { Store } from "./Redux/Store";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
+import { store } from "./Redux/stores/store";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Provider store={Store}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
