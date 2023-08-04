67bb62c32acb94c205f52bb81e155b2d0209be96
Raman Verma <devramanverma@gmail.com>
2023-08-04 18:30:35 +0530

    Added logo for dark and light theme

---
+++ src/Components/Logos/Logo.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../../Providers/ThemeProvider';
+
+function Logo(props) {
+    const themeContext = useContext(ThemeContext);
+    
+    const logos = {
+        dark: '/assets/images/logos/logo-dark.png', 
+        light: '/assets/images/logos/logo-light.png' 
+    };
+
+    return (
+        <img src={logos[themeContext.theme]} {...props}>
+            {props.children}
+        </img>
+    );
+};
+
+export default Logo;
