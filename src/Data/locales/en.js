c3929f3aa058d998bc8480b7c444505e01534239
Raman Verma <devramanverma@gmail.com>
2023-08-05 00:38:56 +0530

    Added locales switch translations

--- src/Data/locales/en.js
+++ src/Data/locales/en.js
@@ -6,5 +6,13 @@
  * @var const Object en
  */
 export const en = {
-    //
+    /**
+     * Switch
+     * 
+     * @name Switch.js
+     * @file src://Components/Locales/Switch.js
+     */
+    "Change": "Change",
+    "Cancel": "Cancel",
+    "Select Your Language": "Select Your Language"
 };
--- src/Data/locales/hi.js
+++ src/Data/locales/hi.js
@@ -6,5 +6,13 @@
  * @var const Object hi
  */
 export const hi = {
-    //
+    /**
+     * Switch
+     * 
+     * @name Switch.js
+     * @file src://Components/Locales/Switch.js
+     */
+    "Change": "परिवर्तन",
+    "Cancel": "रद्द करना",
+    "Select Your Language": "अपनी भाषा का चयन करें"
 };
