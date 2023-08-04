6c47069596f67bc77dfaad299dabd72091009bcc
Raman Verma <devramanverma@gmail.com>
2023-08-05 00:39:32 +0530

    Added about us page translations

--- src/Data/locales/en.js
+++ src/Data/locales/en.js
@@ -14,5 +14,20 @@ export const en = {
      */
     "Change": "Change",
     "Cancel": "Cancel",
-    "Select Your Language": "Select Your Language"
+    "Select Your Language": "Select Your Language",
+
+    /**
+     * About Us
+     * 
+     * @name AboutUs.js
+     * @file src://Pages/AboutUs/AboutUs.js
+     */
+    "ResumeBuilder": "ResumeBuilder",
+    "Share with Your Friends.": "Share with Your Friends.",
+    "Welcome to our Resume Builder!": "Welcome to our Resume Builder!",
+    "We understand the importance of creating a professional and impactful resume.": "We understand the importance of creating a professional and impactful resume.",
+    "If you call failures experiments, you can put them in your resume and claim them as achievements": "If you call failures experiments, you can put them in your resume and claim them as achievements",
+    "Our Resume Builder is designed to simplify the resume creation process, offering a user-friendly interface and a wide range of customizable templates.": "Our Resume Builder is designed to simplify the resume creation process, offering a user-friendly interface and a wide range of customizable templates.",
+    "We believe that a well-crafted resume can make a significant difference in your job search, helping you stand out from the competition and land your dream job.": "We believe that a well-crafted resume can make a significant difference in your job search, helping you stand out from the competition and land your dream job.",
+    "Whether you\'re a recent graduate, a seasoned professional, or making a career transition, our platform provides the tools and resources you need to create a compelling resume that highlights your skills, experience, and achievements.": "Whether you\'re a recent graduate, a seasoned professional, or making a career transition, our platform provides the tools and resources you need to create a compelling resume that highlights your skills, experience, and achievements.",
 };
--- src/Data/locales/hi.js
+++ src/Data/locales/hi.js
@@ -14,5 +14,20 @@ export const hi = {
      */
     "Change": "परिवर्तन",
     "Cancel": "रद्द करना",
-    "Select Your Language": "अपनी भाषा का चयन करें"
+    "Select Your Language": "अपनी भाषा का चयन करें",
+    
+    /**
+     * About Us
+     * 
+     * @name AboutUs.js
+     * @file src://Pages/AboutUs/AboutUs.js
+     */
+    "ResumeBuilder": "ResumeBuilder",
+    "Share with Your Friends.": "अपने दोस्तों के साथ साझा करें.",
+    "Welcome to our Resume Builder!": "हमारे रिज्यूमे बिल्डर में आपका स्वागत है!",
+    "We understand the importance of creating a professional and impactful resume.": "हम एक पेशेवर और प्रभावशाली फिर से शुरू करने के महत्व को समझते हैं.",
+    "If you call failures experiments, you can put them in your resume and claim them as achievements": "यदि आप विफलताओं के प्रयोगों को कहते हैं, तो आप उन्हें अपने फिर से शुरू कर सकते हैं और उन्हें उपलब्धियों के रूप में दावा कर सकते हैं",
+    "Our Resume Builder is designed to simplify the resume creation process, offering a user-friendly interface and a wide range of customizable templates.": "हमारे रिज्यूमे बिल्डर को फिर से शुरू करने की प्रक्रिया को सरल बनाने के लिए डिज़ाइन किया गया है, जो उपयोगकर्ता के अनुकूल इंटरफेस और अनुकूलन योग्य टेम्पलेट्स की एक विस्तृत श्रृंखला पेश करता है.",
+    "We believe that a well-crafted resume can make a significant difference in your job search, helping you stand out from the competition and land your dream job.": "हमारा मानना है कि एक अच्छी तरह से तैयार किया गया रिज्यूमे आपकी नौकरी की खोज में महत्वपूर्ण अंतर ला सकता है, जिससे आपको प्रतियोगिता से बाहर खड़े होने और अपने सपनों की नौकरी को पूरा करने में मदद मिलेगी.",
+    "Whether you\'re a recent graduate, a seasoned professional, or making a career transition, our platform provides the tools and resources you need to create a compelling resume that highlights your skills, experience, and achievements.": "चाहे आप हाल ही में स्नातक हों, एक अनुभवी पेशेवर हों, या एक कैरियर संक्रमण बना रहे हों, हमारा मंच आपको एक आकर्षक फिर से शुरू करने के लिए आवश्यक उपकरण और संसाधन प्रदान करता है जो आपके कौशल को उजागर करता है, अनुभव, और उपलब्धियां.",
 };
