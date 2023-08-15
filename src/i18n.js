238005466c0fc702db4f7a31d75b6e1565ea60f1
Raman Verma <devramanverma@gmail.com>
2023-08-15 13:56:33 +0530

    Updated double inverted comma to single inverted comma

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,11 @@
 import i18n from 'i18next';
-import { initReactI18next } from "react-i18next";
-import { config, resources } from "./Data/translation";
-import { localStorage as ls} from "./Modules/LocalStorageSlim";
+import { initReactI18next } from 'react-i18next';
+import { config, resources } from './Data/translation';
+import { localStorage as ls} from './Modules/LocalStorageSlim';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-config.selected = ls.get('locale',{ 
-    decrypt: true 
+config.selected = ls.get('locale',{
+    decrypt: true
 });
 
 config.selected = (
