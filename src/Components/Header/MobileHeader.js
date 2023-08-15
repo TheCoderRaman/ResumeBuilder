e7f96fb0b0934211b6dbaae6f550aa83d474727d
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:07:25 +0530

    Added link text translation and also fixed import path

--- src/Components/Header/MobileHeader.js
+++ src/Components/Header/MobileHeader.js
@@ -1,30 +1,44 @@
+import {
+    NavLink,
+    useNavigate
+} from 'react-router-dom';
+
 import React from 'react';
-import Logo from '../Logos/Logo';
+import Logo from './../Logos/Logo';
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
+import Divider from '@mui/material/Divider';
+import { route } from './../../Utils/route';
 import ThemeSwitch from './../Themes/Switch';
+import { useTranslation } from 'react-i18next';
 import LocaleSwitch from './../Locales/Switch';
 import { routes } from './../../Data/Web/routes';
-import { Link, NavLink } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 
 function MobileHeader({
     onClick
 }) {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
     return (
         <Box onClick={onClick} sx={{ textAlign: "center" }}>
             <Typography variant="h6" sx={{ my: 2 }}>
-                <Link to="/templete" className="flex items-center p-2 opacity-85">
+                <div
+                    onClick={() => {navigate(route('Home'))}}
+                    className="flex items-center p-2 opacity-85 w-[100%] h-auto"
+                >
                     {" "}
                     <Logo
                         width="100%"
                         height="auto"
                         alt="Almabetter Logo"
                     />
-                </Link>
+                </div>
             </Typography>
+
             <Divider />
+            
             <List
                 className="drawerLinks"
                 sx={{
@@ -46,7 +60,7 @@ function MobileHeader({
                             className="nav-link"
                             key={`DesktopLink#${name}::${index}`}
                         >
-                            {value?.name}
+                            {t(value?.name)}
                         </NavLink>
                     );
                 })}
