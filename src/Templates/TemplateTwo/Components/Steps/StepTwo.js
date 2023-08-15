a3800c1dc849916664c7ea4ab1ae346bbbf2f481
Raman Verma <devramanverma@gmail.com>
2023-08-15 15:43:36 +0530

    Added step two component

---
+++ src/Templates/TemplateTwo/Components/Steps/StepTwo.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import get from 'lodash/get';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { useTranslation } from 'react-i18next';
+import Typography from '@mui/material/Typography';
+import ListItemText from '@mui/material/ListItemText';
+
+function StepTwo({ form, themes, limit, templateForm }) {
+  const { palette } = themes;
+  const { t } = useTranslation();
+
+  return (
+    <List
+      dense={true}
+      sx={{
+        maxWidth: 360,
+        width: '100%',
+        scale: '1 0.8',
+        backgroundColor: palette.secondary.main,
+      }}
+    >
+      {Object.entries(get(form,'stepTwo.data')).map(
+        ([key,value],index) => {
+          if(index >= limit){
+            return;
+          }
+
+          let startYear = (Object
+            .values(get(templateForm,'stepTwo.inputs'))
+            .filter((value) => value?.type === 'fieldArray')?.at(0)?.inputs
+            .filter((value) => value?.name === 'start_year')?.at(0)?.props?.options
+          );
+
+          let endYear = (Object
+            .values(get(templateForm,'stepTwo.inputs'))
+            .filter((value) => value?.type === 'fieldArray')?.at(0)?.inputs
+            .filter((value) => value?.name === 'start_year')?.at(0)?.props?.options
+          );
+
+          return (
+            <ListItem key={`#Experiences:${index}-${key}`} alignItems="flex-start">
+              
+              <ListItemText
+                sx={{
+                  fontWeight: 'bold',
+                  color: palette.tertiary.main
+                }}
+                primary={get(value,'job_title')}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      variant="body2"
+                      component="span"
+                      sx={{
+                        display: 'inline',
+                        fontStyle: 'normal',
+                        color: palette.quaternary.main,
+                      }}
+                    >
+                      {get(value,'organization_name')}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+
+              <Typography sx={{ fontStyle: 'italic', color: palette.tertiary.main }}>
+                {startYear[get(value,'start_year')]} {' - '} {endYear[get(value,'end_year')]}
+              </Typography>
+            </ListItem>
+          );
+        }
+      )}
+    </List>
+  )
+}
+
+export default StepTwo;
