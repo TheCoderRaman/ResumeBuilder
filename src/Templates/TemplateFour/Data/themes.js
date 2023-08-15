7d24809ae74215ae2de9ade0e5d25dedce1ce9fd
Raman Verma <devramanverma@gmail.com>
2023-08-15 15:51:55 +0530

    Added template four themes

---
+++ src/Templates/TemplateFour/Data/themes.js
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+
+/**
+ * All template four themes will be defined here.
+ *
+ * @var const themes
+ */
+export const themes = createTheme({
+  palette: {
+    primary: {
+        main: '#403D3C',
+    },
+    secondary: {
+        main: '#62595A',
+    },
+    tertiary: {
+        main: '#FFE3E7',
+    },
+    quaternary: {
+        main: '#3A36BF',
+    },
+    quinary: {
+        main: '#BF4141',
+    }
+  },
+});
