1624f1a0a90f6c9c55fd03bcbfeabd5db4432060
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:17:32 +0530

    Added application action progress

---
+++ src/Data/actions/progress.js
@@ -0,0 +1,10 @@
+/**
+ * All application action progress will be defined here.
+ *
+ * @var const progress
+ */
+export const progress = {
+    CLOSED: 'closed',
+    OPENED: 'opened',
+    PENDING: 'pending'
+};
