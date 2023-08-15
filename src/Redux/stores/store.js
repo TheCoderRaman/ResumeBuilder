5a4fe75db34efc0acacedf217460bb137f641fcb
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:13:17 +0530

    Added forms slice in redux store

--- src/Redux/stores/store.js
+++ src/Redux/stores/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import formsSlice from './../features/forms/formsSlice';
 import settingsSlice from './../features/settings/settingsSlice';
 
 export const store = configureStore({
   reducer: {
+    forms: formsSlice,
     settings: settingsSlice,
   },
 });
