eaaae896c9fdeff3a03783ee6a0286f7ca9849b8
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:13:47 +0530

    Added templates slice in redux store

--- src/Redux/stores/store.js
+++ src/Redux/stores/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formsSlice from './../features/forms/formsSlice';
 import settingsSlice from './../features/settings/settingsSlice';
+import templatesReducer from './../features/templates/templatesSlice';
 
 export const store = configureStore({
   reducer: {
     forms: formsSlice,
     settings: settingsSlice,
+    templates: templatesReducer
   },
 });
