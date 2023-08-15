dcadffebb367d64cf616f3908eda8dcffea5cb5a
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:42:27 +0530

    Added resume templates

---
+++ src/Data/templates/templates.js
@@ -0,0 +1,39 @@
+/**
+ * All application resume templates will be defined here.
+ *
+ * @var const templates
+ */
+export const templates = {
+  selected: 'one',
+  forms: {
+      one: {
+          name: 'Template One',
+          form: 'tech_resume',
+          thumbnail: '/assets/images/templates/template-1.png'
+      },
+      two: {
+          id: 2,
+          name: 'Template Two',
+          form: 'tech_resume',
+          thumbnail: '/assets/images/templates/template-2.png'
+      },
+      three: {
+          id: 3,
+          name: 'Template Three',
+          form: 'tech_resume',
+          thumbnail: '/assets/images/templates/template-3.png'
+      },
+      four: {
+          id: 4,
+          name: 'Template Four',
+          form: 'tech_resume',
+          thumbnail: '/assets/images/templates/template-4.png'
+      },
+      five: {
+          id: 5,
+          name: 'Template Five',
+          form: 'tech_resume',
+          thumbnail: '/assets/images/templates/template-5.png'
+      }
+  }
+};
