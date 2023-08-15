3cc050826a9875df9a29848a08e3ffacda8a827f
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:20:49 +0530

    Added application forms storage

---
+++ src/Data/forms/storages.js
@@ -0,0 +1,12 @@
+/**
+ * All application resume forms storages will be defined here.
+ *
+ * @var const storages
+ */
+export const storages = {
+    selected: null,
+    storages: {
+        //
+    },
+    generateSelected: null,
+};
