a9daec83a48411ab618d16d2ddc695d1e96f6b65
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:12:05 +0530

    Fixed import paths

--- src/Redux/stores/store.js
+++ src/Redux/stores/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import settingReducer from "./../features/settings/settingsSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import settingsSlice from './../features/settings/settingsSlice';
 
 export const store = configureStore({
   reducer: {
-    settings: settingReducer
+    settings: settingsSlice,
   },
 });
