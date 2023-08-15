5288b844beb5d7081cfdfd6666bc92ef528a8abb
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:41:17 +0530

    Added pagination config

---
+++ src/Data/paginations/paginate.js
@@ -0,0 +1,16 @@
+/**
+ * All application pagination config will be defined here.
+ *
+ * @var const paginate
+ */
+export const paginate = {
+    config: {
+        pages: 0,
+        perPage: 0,
+        pageNumber: 0,
+    },
+    data: {
+        items: [],
+        currentItems: []
+    }
+};
