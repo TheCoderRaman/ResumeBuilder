fbc2b94b45d656df3a3fc1212811140c8ec80d68
Raman Verma <devramanverma@gmail.com>
2023-08-15 15:47:41 +0530

    Added template three themes

---
+++ src/Templates/TemplateThree/Data/themes.js
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+
+/**
+ * All template three themes will be defined here.
+ *
+ * @var const themes
+ */
+export const themes = createTheme({
+  palette: {
+    primary: {
+        main: '#FFFFFF',
+    },
+    secondary: {
+        main: '#E3E3E3',
+    },
+    tertiary: {
+        main: '#000000',
+    },
+    quaternary: {
+        main: '#034C8C',
+    },
+    quinary: {
+        main: '#E3E1D5',
+    }
+  },
+});
