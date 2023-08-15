3b5c85ed3e2d5bdff8cd1d476b1b0a35292bed48
Raman Verma <devramanverma@gmail.com>
2023-08-15 15:41:40 +0530

    Added template two themes

---
+++ src/Templates/TemplateTwo/Data/themes.js
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+
+/**
+ * All template two themes will be defined here.
+ *
+ * @var const themes
+ */
+export const themes = createTheme({
+  palette: {
+    primary: {
+        main: '#272932',
+    },
+    secondary: {
+        main: '#828489',
+    },
+    tertiary: {
+        main: '#BF4141',
+    },
+    quaternary: {
+        main: '#B6C2D9',
+    },
+    quinary: {
+        main: '#4D7EA8',
+    }
+  },
+});
