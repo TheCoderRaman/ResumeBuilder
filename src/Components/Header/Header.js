eed47149a5809c8b10d16a3a3e17b6c1704b949e
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:07:07 +0530

    Added link text translation and also fixed import path

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,29 @@
 import './Header.css';
+
+import {
+  NavLink,
+  useNavigate
+} from 'react-router-dom';
+
 import * as React from 'react';
-import Logo from '../Logos/Logo';
+import Logo from './../Logos/Logo';
 import Box from '@mui/material/Box';
 import MobileHeader from './MobileHeader';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
+import { route } from './../../Utils/route';
 import Toolbar from '@mui/material/Toolbar';
 import ThemeSwitch from './../Themes/Switch';
+import { useTranslation } from 'react-i18next';
 import LocaleSwitch from './../Locales/Switch';
 import MenuIcon from '@mui/icons-material/Menu';
 import { routes } from './../../Data/Web/routes';
-import { Link, NavLink } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 
 function Header() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
@@ -24,13 +33,13 @@ function Header() {
   return (
     <>
       <Box sx={{ display: "flex" }}>
-        <AppBar 
-            component="nav" 
-            position="sticky" 
-            className="appbar" 
+        <AppBar
+            component="nav"
+            position="sticky"
+            className="appbar"
             sx={{
               boxShadow: 3,
-              color: 'text.primary', 
+              color: 'text.primary',
               bgcolor: 'background.default'
             }}
         >
@@ -42,6 +51,7 @@ function Header() {
               sx={{ mr: 2, display: { sm: "none" } }}>
               <MenuIcon />
             </IconButton>
+
             <Typography
               variant="h6"
               component="div"
@@ -52,15 +62,19 @@ function Header() {
                 position: "relative",
                 top: "5px",
               }}>
-              <Link to="/templete" className="homeIcon flex items-center opacity-85">
+              <div
+                onClick={() => {navigate(route('Home'))}}
+                className="homeIcon flex items-center opacity-85 w-[150px] h-auto"
+              >
                 {" "}
                 <Logo
                   width="150px"
                   height="auto"
                   alt="Almabetter Logo"
                 />
-              </Link>
+              </div>
             </Typography>
+
             <Box color="inherit" sx={{ display: { xs: "none", sm: "block" }}}>
               {Object.entries(routes).map(([name, value], index) => {
                 if(true !== value?.public){
@@ -73,16 +87,18 @@ function Header() {
                      className="nav-link"
                      key={`DesktopLink#${name}::${index}`}
                   >
-                    {value?.name}
+                    {t(value?.name)}
                   </NavLink>
                 );
               })}
 
             </Box>
+
             <ThemeSwitch />
             <LocaleSwitch />
           </Toolbar>
         </AppBar>
+
         <Box component="nav">
           <Drawer
             variant="temporary"
