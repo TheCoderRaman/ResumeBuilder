fb27893a28324d6f3e46abd070cfdfb2bba75a34
Raman Verma <devramanverma@gmail.com>
2023-08-15 15:48:13 +0530

    Added left component

---
+++ src/Templates/TemplateThree/Components/Left/Left.js
@@ -0,0 +1,38 @@
+import {
+  Box,
+} from '@mui/material';
+
+import React from 'react';
+import Profile from './Profile';
+import StepOne from './../Steps/StepOne';
+import StepFour from './../Steps/StepFour';
+
+function Left(props) {
+  const { palette } = props.themes;
+
+  return (
+    <Box
+      sx={{
+        width: "36%",
+        float: "left",
+        height: "113vh",
+        padding: "1% 1%",
+        backgroundColor: palette.secondary.main,
+      }}
+    >
+      <div className='px-0'>
+        <Profile {...props}/>
+      </div>
+      
+      <div className='px-0'>
+        <StepOne {...props} />
+      </div>
+
+      <div className='px-0'>
+        <StepFour {...props} />
+      </div>
+    </Box>
+  );
+}
+
+export default Left;
