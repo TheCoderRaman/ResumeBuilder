21ed4b751d9c821c504ed252c367dd2da5cb9cf2
Raman Verma <devramanverma@gmail.com>
2023-08-15 14:20:01 +0530

    Added application form steps

---
+++ src/Data/forms/steps.js
@@ -0,0 +1,16 @@
+/**
+ * All application steps will be defined here.
+ *
+ * @var const steps
+ */
+export const steps = {
+    config: {
+        step: 0,
+        steps: {},
+        status: {},
+    },
+    data: {
+        inputs: {},
+        filled: {}
+    }
+};
